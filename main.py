cabac9bb8bab26c6c718e62309f679c911e188c0
umair <umairalism@gmail.com>
2024-01-25 22:10:01 +0500

    updated gitignore file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from waterQualityPrediction.pipeline.stage_02_data_ingestion import DataIngestionTrainingPipeline
-from waterQualityPrediction.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from waterQualityPrediction.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from waterQualityPrediction.pipeline.stage_02_data_transformation import DataTransformationTrainingPipeline
 from waterQualityPrediction import logger
 
 STAGE_NAME = "Data Ingestion Stage"
