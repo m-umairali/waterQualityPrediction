b3c4d03942fe25dcd2c3e04961661ef6a7774a34
umair <umairalism@gmail.com>
2024-01-27 19:27:25 +0500

    model training stage completed final

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ except Exception as e:
 STAGE_NAME = "Data Transformation Stage"
 try:
     logger.info(f">>>> Stage {STAGE_NAME} started<<<<")
-    data_ingestion = DataTransformationTrainingPipeline()
-    data_ingestion.main()
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
     logger.info(f">>>> Stage {STAGE_NAME} completed <<<<< \n\nx======x")
 except Exception as e:
     logger.info(e)
@@ -29,7 +29,7 @@ STAGE_NAME = "Model Training Stage"
 try:
     logger.info(f">>>> Stage {STAGE_NAME} started<<<<")
     model_trainer = ModelTrainingPipeline()
-    data_ingestion.main()
+    model_trainer.main()
     logger.info(f">>>> Stage {STAGE_NAME} completed <<<<< \n\nx======x")
 except Exception as e:
     logger.info(e)
